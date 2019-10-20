acd512bcbb8e7f2903c599708e377920f986f5ee
Andrew Lu <andrew.yunxiang@gmail.com>
2019-10-19 17:36:52 -0700

    Initial drivetrain subsystem commit, added utils from road-runner-quickstart https://github.com/acmerobotics/road-runner-quickstart

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Subsystem.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+
+
+public interface Subsystem {
+
+    /**
+     * Ocean man take me by the hand take me to the land
+     */
+    void update();
+}
